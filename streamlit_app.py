6825adc7ae777d4bcd1a8ad5683fcf489dde0561
Soumya Jindal <98908906+soumyajindal0707@users.noreply.github.com>
2023-12-21 20:35:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,29 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
+import spacy
 
-"""
-# Welcome to Streamlit!
+# Load spaCy model
+nlp = spacy.load("en_core_web_sm")
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# Function to process user input and generate a response
+def process_input(user_input):
+    doc = nlp(user_input)
+    # Add your logic for generating a response based on user input
+    # For simplicity, the chatbot just echoes the user input in this example
+    response = "You said: " + user_input
+    return response
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# Streamlit app
+def main():
+    st.title("Simple Chatbot")
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+    # Text input for user to enter messages
+    user_input = st.text_input("Enter your message:")
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+    if st.button("Send"):
+        # Process user input and get response
+        response = process_input(user_input)
+        # Display response
+        st.text("Bot: " + response)
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+if __name__ == "__main__":
+    main()
